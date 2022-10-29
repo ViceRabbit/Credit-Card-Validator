87b460b0ff09d432b2c9e33f0f69d75f3ca4f5c4
ViceRabbit <vicerabbit16789@gmail.com>
2022-10-29 01:22:04 +0000

    Finished Credit Card Validator :) - 10/28/2022

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ for num in CardNum[::-2]: # odd
 for num in CardNum[-2::-2]: # even
     num = int(num) * 2
     if num > 9:
-        num = 1 + num%10 # as 9 is the highest num, 9*2 = 18, first num will always be 1 
+        num = 1 + num%10 # as 9 is the highest num, 9*2 = 18, first num will always be 1. 
     value += num
 
 if value % 10 == 0:
